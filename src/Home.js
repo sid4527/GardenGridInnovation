8fb85a2071eccb6728a14fb3b1ddb842d1ae0229
sbaksh4527 <78521742+sbaksh4527@users.noreply.github.com>
2024-10-29 21:56:32 -0400

    Updating navigation bar for home page

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,6 @@ function Home() {
   return (
     <div className="home">
 
-      <nav className="tabs">
-        <button className="tab">Home</button>
-        <button className="tab">Inventory</button>
-        <button className="tab">Entries</button>
-        <button className="tab">Resources</button>
-        <button className="tab">Reports</button>
-
-      </nav>
-
       <main className="main-content">
         <p>** Home page image array will be inserted here **</p>
       </main>
