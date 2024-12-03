9bb64202b635e059f39c02d4ad1da2ca5ba50799
sbaksh4527 <78521742+sbaksh4527@users.noreply.github.com>
2024-12-03 03:11:26 -0500

    Updating so it can be deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { FutureConfigProvider } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <FutureConfigProvider v7_startTransition v7_relativeSplatPath>
+  <Router>
     <App />
-  </FutureConfigProvider>,
+  </Router>,
   document.getElementById('root')
 );
